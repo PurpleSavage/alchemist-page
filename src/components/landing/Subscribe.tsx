fb131b44db63db0cec21562073dfb907eff794e5
PurpleSavage <jzuritapalomino@gmail.com>
2025-02-14 10:48:23 -0500

    agregando funcionalidad de wsp

--- src/components/landing/Subscribe.tsx
+++ src/components/landing/Subscribe.tsx
@@ -1,8 +1,8 @@
 import style from '../../css/landing/subscribe.module.css'
-import Image from 'next/image'
+import SubscribeForm from './SubscribeForm'
 export default function Subscribe() {
   return (
-    <div className='overflow-hidden relative  mx-auto w-[80%] h-[500px]  min-h-screen '>
+    <section className='overflow-hidden relative  mx-auto w-[80%] h-[500px]  min-h-screen '>
         <div className={`${style.meteorContainer}`}>
             <div className={`${style.meteor}`}></div>
             <div className={`${style.meteor}`}></div>
@@ -13,46 +13,8 @@ export default function Subscribe() {
         <div className='flex flex-col items-center justify-center absolute bottom-0 left-0 
         right-0 top-0 z-10 gap-16'>
             <h2 className='text-5xl font-bold'>Regístrate y forma parte de Alchemist</h2>
-            <form className={`${style.form}  border-2 border-slate-500/50 p-4 w-2/5 space-y-4 rounded-md`}>
-                <div className='w-full justify-center flex'>
-                    <Image
-                        width={80}
-                        height={25}
-                        src={'/logo.png'}
-                        alt='triangulo '
-                    />
-                </div>
-                <div className='space-y-2'>
-                    <label htmlFor="" className='block'>Nombre</label>
-                    <input 
-                        type="text" 
-                        placeholder='Nombre' 
-                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md'
-                    />
-                </div>
-                <div className='space-y-2'>
-                    <label htmlFor="" className='block'>Email</label>
-                    <input 
-                        type="text" 
-                        placeholder='Email' 
-                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md'
-                    />
-                </div>
-                <div className='space-y-2'>
-                    <label htmlFor="" className='block'>Número de teléfono</label>
-                    <input 
-                        type="text" 
-                        placeholder='933256487' 
-                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md' 
-                    />
-                </div>
-                <button 
-                    type='submit' 
-                    className='w-full text-center text-white py-2 rounded-md hover:bg-yellow-600
-                     bg-yellow-500 shadow-lg shadow-yellow-500/50'
-                >Enviar</button>
-            </form> 
+            <SubscribeForm/>
         </div>
-    </div>
+    </section>
   )
 }
---
+++ src/components/landing/SubscribeForm.tsx
@@ -0,0 +1,71 @@
+'use client'
+import Image from "next/image";
+import style from '../../css/landing/subscribe.module.css'
+import { FormEvent, useState } from "react";
+
+export default function SubscribeForm() {
+    const [nombre,setNombre]=useState("")
+    const [email,setEmail]=useState("")
+    const [phone,setPhone]=useState("")
+    const [error,setError]=useState("")
+
+    const sendMessage = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if([nombre,email,phone].includes('')){
+            setError("hay campos incompletos")
+            return
+        }
+        const phoneNumber = "979797894"; 
+        const message = encodeURIComponent(
+            `Hola, mi nombre quisiera unirme al grupo, vengo de la web de 
+            Alchemist.\n\nEstos son mis datos:\nEmail: ${email}\nNombre: ${nombre}\nCelular: ${phone}`
+        );
+          
+        window.open(`https://wa.me/${phoneNumber}?text=${message}`, "_blank");
+    };
+  return (
+    <form className={`${style.form}  border-2 border-slate-500/50 p-4 w-2/5 space-y-4 rounded-md`} onSubmit={sendMessage}>
+                <div className='w-full justify-center flex'>
+                    <Image
+                        width={80}
+                        height={25}
+                        src={'/logo.png'}
+                        alt='triangulo '
+                    />
+                </div>
+                {error && <p className="py-1 w-full bg-red-600 text-center rounded-md text-white font-bold">{error}</p>}
+                <div className='space-y-2'>
+                    <label htmlFor="" className='block'>Nombre</label>
+                    <input 
+                        type="text" 
+                        placeholder='Nombre' 
+                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md'
+                        onChange={(e)=>setNombre(e.target.value)}
+                    />
+                </div>
+                <div className='space-y-2'>
+                    <label htmlFor="" className='block'>Email</label>
+                    <input 
+                        type="text" 
+                        placeholder='Email' 
+                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md'
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='space-y-2'>
+                    <label htmlFor="" className='block'>Número de teléfono</label>
+                    <input 
+                        type="text" 
+                        placeholder='933256487' 
+                        className='bg-transparent border border-slate-500/50 h-10 w-full px-2 rounded-md' 
+                        onChange={(e)=>setPhone(e.target.value)}
+                    />
+                </div>
+        <button 
+            type='submit' 
+            className='w-full text-center text-white py-2 rounded-md hover:bg-yellow-600
+            bg-yellow-500 shadow-lg shadow-yellow-500/50'
+        >Enviar</button>
+    </form>
+  )
+}
